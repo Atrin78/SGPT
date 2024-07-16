f2d91bb06ab40f91ae1c40f6e2026b67819025b8
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2024-07-16 06:47:36 +0200

    SFDS

--- train.sh
+++ train.sh
@@ -21,7 +21,8 @@
 
 # domainnet
 python3 main_prompt.py  --pretrained_dir checkpoints/imagenet21k_ViT-B_16.npz --model_type ViT-B_16 \
-          --n_parties 6 --cls_num 10 --device cuda:4 \
-         --batch-size 50 --comm_round 50  --test_round 15 --sample 1 --rho 0.9 --alg SGPT\
+          --n_parties 6 --cls_num 10 --device cuda:3 \
+         --batch-size 50 --comm_round 50  --test_round 20 --sample 1 --rho 0.9 --alg SGPT\
         --dataset domainnet --lr 0.01 --epochs 5 --key_prompt 6 --avg_key --moment 0 \
+        --share_blocks_g 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15\
           --tta --datadir /mnt/nas-server/workspace/atrin/
